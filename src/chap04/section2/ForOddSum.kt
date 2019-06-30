51b434d056f88fa7c47f9286c329f1b9101b2fc0
youngkiu <youngkiu@gmail.com>
2019-06-30 16:47:42 +0900

    Update ForOddSum.kt
    "Let's use the for statement that uses step to find the sum of odd numbers and the sum of even numbers from 1 to 100 respectively." So, should not it be modified as above?

--- src/chap04/section2/ForOddSum.kt
+++ src/chap04/section2/ForOddSum.kt
@@ -4,10 +4,11 @@ fun main() {
 
     var total: Int = 0
 
-    for (num in 1..100 step 2) total += num
+    for (num in 1..99 step 2) total += num
     println("Odd total: $total")
 
-    for (num in 0..99 step 2) total += num
+    total = 0
+    for (num in 0..100 step 2) total += num
     println("Even total: $total")
 
 }
